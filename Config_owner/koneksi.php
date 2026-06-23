c0ed57471ce8398f968de1effcc2950a71556f42
tiara mailani ruswandi <tiaramailani207@gmail.com>
2026-06-23 10:45:53 +0700

    Add files via upload

---
+++ Config_owner/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "db_wishpywash"
+);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+echo "Koneksi Berhasil";
+?>
