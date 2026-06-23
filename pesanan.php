30f75e18fe430ee38869945a9b3e21a40b7c20e5
tiara mailani ruswandi <tiaramailani207@gmail.com>
2026-06-23 10:29:02 +0700

    Add files via upload

---
+++ pesanan.php
@@ -0,0 +1,30 @@
+<?php
+include "../Config/koneksi.php";
+
+header("Content-Type: application/json");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+
+    $data = mysqli_query($conn, "SELECT * FROM orders");
+
+    $result = [];
+    while($row = mysqli_fetch_assoc($data)){
+        $result[] = $row;
+    }
+
+    echo json_encode($result);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'PUT'){
+
+    parse_str(file_get_contents("php://input"), $put);
+
+    $id = $put['id'];
+    $status = $put['status'];
+
+    mysqli_query($conn,
+    "UPDATE orders SET status='$status' WHERE id='$id'");
+
+    echo json_encode(["message"=>"Status updated"]);
+}
+?>
